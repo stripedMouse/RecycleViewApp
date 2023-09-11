4e931aa471519705dc6e6a2423763e2f6602bd63
BeMou <bel.kostya@yandex.ru>
2023-09-11 08:55:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recycleview/DatchikAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.recycleview
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class DatchikAdapter: RecyclerView.Adapter<DatchikAdapter.DatchikHolder> {
+
+    class DatchikHolder(item: View): RecyclerView.ViewHolder(item) {
+
+        fun bind(datchik: Datchik){
+
+        }
+
+    }
+
+}
