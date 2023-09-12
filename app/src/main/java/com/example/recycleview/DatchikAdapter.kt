633a2911a42d44c7d774916a5b2f4c7c624b48ea
BELLL <bel.kostya@yandex.ru>
2023-09-12 21:53:31 +0300

    final

--- app/src/main/java/com/example/recycleview/DatchikAdapter.kt
+++ app/src/main/java/com/example/recycleview/DatchikAdapter.kt
@@ -1,16 +1,41 @@
 package com.example.recycleview
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recycleview.databinding.DatchikItemBinding
 
-class DatchikAdapter: RecyclerView.Adapter<DatchikAdapter.DatchikHolder> {
+class DatchikAdapter: RecyclerView.Adapter<DatchikAdapter.DatchikHolder>() {
+    val datchikList = ArrayList<Datchik>()
 
     class DatchikHolder(item: View): RecyclerView.ViewHolder(item) {
+        val binding = DatchikItemBinding.bind(item)
 
-        fun bind(datchik: Datchik){
+        fun bind(datchik: Datchik) = with(binding){
+            im.setImageResource(datchik.imageId)
+            tvTitle.text = datchik.name
 
         }
 
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DatchikHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.datchik_item, parent, false)
+        return DatchikHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return datchikList.size
+    }
+
+    override fun onBindViewHolder(holder: DatchikHolder, position: Int) {
+        holder.bind(datchikList[position])
+    }
+
+    public fun addDatchik(datchik: Datchik){
+        datchikList.add(datchik)
+        notifyDataSetChanged()
     }
 
 }
--- app/src/main/java/com/example/recycleview/MainActivity.kt
+++ app/src/main/java/com/example/recycleview/MainActivity.kt
@@ -2,15 +2,42 @@ package com.example.recycleview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recycleview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val adapter = DatchikAdapter()
+    private val imageIdList = listOf(
+        R.drawable.bi,
+        R.drawable.or,
+        R.drawable.ipd,
+        R.drawable.ipr,
+        R.drawable.ipt,
+    )
+    private var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        init()
+    }
+
+    private fun init(){
+        binding.apply {
+
+        rcView.layoutManager = GridLayoutManager(this@MainActivity, 3)
+        rcView.adapter = adapter
+        bAdd.setOnClickListener {
+            if(counter > 4) counter = 0
+            val datchik = Datchik(imageIdList[counter], "Datchik $counter")
+            adapter.addDatchik(datchik)
+            counter++
+        }
+
+        }
     }
 }
